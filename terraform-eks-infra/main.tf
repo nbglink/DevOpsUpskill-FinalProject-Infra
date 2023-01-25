b155e150e8de8a8b314a46c40b60426f9169355b
Hristo <nbglink@gmail.com>
2023-01-25 08:45:48 +0000

    ready

--- terraform-eks-infra/main.tf
+++ terraform-eks-infra/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "devops-upskill-eks-${random_string.suffix.result}"
+  cluster_name = "devops-upskill-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
